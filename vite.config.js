cb522cf3606d5aaf0c708f3624565e89531ef1f6
IngridKao <ingrid7613@gmail.com>
2023-03-24 07:55:34 +0800

    切出一個九宮格

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,15 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const processEnvDev = (env) => (env && env.NODE_ENV === 'development')? '': '/neutec-interview/' 
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+	base: processEnvDev(process.env),
+	plugins: [vue()],
+	resolve: {
+		alias: {
+			'@': fileURLToPath(new URL('./src', import.meta.url))
+		}
+	}
 })
