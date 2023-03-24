dc3424deda24b27b9d4470655dbeecc8638dc108
IngridKao <ingrid7613@gmail.com>
2023-03-24 08:09:02 +0800

    部分格子閃爍，指定格子上有垂直水平置中的實心圓

--- src/App.vue
+++ src/App.vue
@@ -7,19 +7,27 @@
 
 	<main>
 		<div class="row">
+			<div class="col">
+				<div class="ball"></div>
+			</div>
 			<div class="col"></div>
-			<div class="col"></div>
-			<div class="col"></div>
+			<div class="col flashing">
+				<div class="ball"></div>
+			</div>
 		</div>
 		<div class="row">
 			<div class="col"></div>
-			<div class="col"></div>
+			<div class="col flashing"></div>
 			<div class="col"></div>
 		</div>
 		<div class="row">
+			<div class="col">
+				<div class="ball"></div>
+			</div>
 			<div class="col"></div>
-			<div class="col"></div>
-			<div class="col"></div>
+			<div class="col flashing">
+				<div class="ball"></div>
+			</div>
 		</div>
 	</main>
 </template>
