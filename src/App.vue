9159e980880396bb0016cb5a84bf291e6dfe62d4
IngridKao <ingrid7613@gmail.com>
2023-04-12 21:06:13 +0800

    改寫：讓格子和球可以被調整

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,25 @@
 <script setup>
     import { reactive } from 'vue'
 	const state = reactive({
-		plusAction: false
+		plusAction: false,
+		boxCount: 3,
+		ballCount: 4,
+		result: [11, 13, 31, 33]
 	})
+	const start = () => {
+		const list = []
+		state.result = []
+		for (let row = 1; row <= state.boxCount; row++) {
+			for (let col = 1; col <= state.boxCount; col++) {
+				list.push(parseInt(`${row}${col}`))
+			}
+		}
+		// random answer
+		for (let row = 0; row <= state.ballCount; row++) {
+			const index = Math.floor(Math.random() * list.length)
+			state.result.push(list[index])
+		}
+	}
 	const plus = () => {
 		state.plusAction = !state.plusAction
 	}
@@ -10,20 +27,24 @@
 
 <template>
 	<header>
-		<button @click="plus">{{state.plusAction? '加分題': '一般移動'}}</button>
+		<label for="boxCount">盒子數量</label>
+		<input type="number" id="boxCount" v-model="state.boxCount">
+		<label for="ballCount">球的數量</label>
+		<input type="number" id="ballCount" v-model="state.ballCount" min="1" :max="state.boxCount*state.boxCount">
+		<button @click="start">Game Start</button>
 	</header>
 
 	<main>
-		<div class="row">
-			<div class="col">
-				<div class="ball move1" :class="{rightDown3: state.plusAction}"></div>
+		<div class="row" v-for="row in state.boxCount" :key="row">
+			<div class="col" v-for="col in state.boxCount" :key="`${row}${col}`">
+				<div v-if="state.result.includes(parseInt(`${row}${col}`))" class="ball move1" :class="{rightDown3: state.plusAction}"></div>
 			</div>
-			<div class="col"></div>
+			<!-- <div class="col"></div>
 			<div class="col flashing">
 				<div class="ball move2" :class="{down3: state.plusAction}"></div>
-			</div>
+			</div> -->
 		</div>
-		<div class="row">
+		<!-- <div class="row">
 			<div class="col"></div>
 			<div class="col flashing"></div>
 			<div class="col"></div>
@@ -36,6 +57,6 @@
 			<div class="col flashing">
 				<div class="ball move2" :class="{down1: state.plusAction}"></div>
 			</div>
-		</div>
+		</div> -->
 	</main>
 </template>
