70a28fb715839ea6a05e2abcdaf17c549b87250a
IngridKao <ingrid7613@gmail.com>
2023-04-12 21:48:26 +0800

    改寫 1. 可以選取盒子，選取後會變色，重複點擊即取消選取 2. 點選game start會隨機選出與球數量相同的答案陣列，如果與選取相同的盒子會閃爍 3. 可以restart
    Next:
    思考球的動畫如何被模組化，目前想法是依據答案來作為end位置，start都從中心上方開始。
    按下restart所有球和之前動畫效果依樣，從左往右跑

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,9 @@
 		plusAction: false,
 		boxCount: 3,
 		ballCount: 4,
-		result: [11, 13, 31, 33]
+		target: [],
+		result: [11, 13, 31, 33],
+		check: []
 	})
 	const start = () => {
 		const list = []
@@ -14,12 +16,32 @@
 				list.push(parseInt(`${row}${col}`))
 			}
 		}
+
 		// random answer
-		for (let row = 0; row <= state.ballCount; row++) {
+		for (let row = 1; row <= state.ballCount; row++) {
 			const index = Math.floor(Math.random() * list.length)
 			state.result.push(list[index])
 		}
+
+		// Check answer
+		state.check = state.target.filter(item => state.result.includes(item))
+
 	}
+	const restart = () => {
+		state.result = []
+		state.target = []
+		state.check = []
+	}
+	const pick = (pickTarget) => {
+		const target = parseInt(pickTarget)
+		const index = state.target.findIndex(item => item == target)
+		if(index < 0){
+			state.target.push(target)
+		}else{
+			state.target = state.target.filter(item => item != target)
+		}
+	}
+	const existCheck = (array, ball) => array.includes(parseInt(ball))
 	const plus = () => {
 		state.plusAction = !state.plusAction
 	}
@@ -32,23 +54,33 @@
 		<label for="ballCount">球的數量</label>
 		<input type="number" id="ballCount" v-model="state.ballCount" min="1" :max="state.boxCount*state.boxCount">
 		<button @click="start">Game Start</button>
+		<button @click="restart">Restart</button>
 	</header>
 
 	<main>
 		<div class="row" v-for="row in state.boxCount" :key="row">
-			<div class="col" v-for="col in state.boxCount" :key="`${row}${col}`">
-				<div v-if="state.result.includes(parseInt(`${row}${col}`))" class="ball move1" :class="{rightDown3: state.plusAction}"></div>
+			<div 
+				v-for="col in state.boxCount" 
+				:key="`${row}${col}`" 
+				class="col"
+				:class="{
+					seleced: existCheck(state.target, `${row}${col}`),
+					flashing: existCheck(state.check, `${row}${col}`)
+				}"
+				@click="pick(`${row}${col}`)"
+			>
+				<div 
+					v-if="existCheck(state.result,`${row}${col}`)" 
+					class="ball move1" 
+					:class="{rightDown3: state.plusAction}"
+				></div>
 			</div>
 			<!-- <div class="col"></div>
 			<div class="col flashing">
 				<div class="ball move2" :class="{down3: state.plusAction}"></div>
 			</div> -->
 		</div>
-		<!-- <div class="row">
-			<div class="col"></div>
-			<div class="col flashing"></div>
-			<div class="col"></div>
-		</div>
+		<!-- 
 		<div class="row">
 			<div class="col">
 				<div class="ball move1" :class="{right3: state.plusAction}"></div>
